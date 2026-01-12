e5552240c735453675d107f71130bbdecaab6cfa
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2026-01-12 22:17:30 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,12 @@
 class Solution {
-    
-    public int solve(int n,int dp[]){
-        if(n<=1)return n;
-        if(dp[n] != -1)return dp[n];
-        int ans = solve(n-1,dp) + solve(n-2,dp);
-        dp[n] = ans;
-        return ans;
-    }
     public int fib(int n) {
-         int[] dp = new int[n+1];
-        for(int i =0;i<=n;i++)dp[i] = -1;
-        return solve(n,dp);
+        if(n == 0)return 0;
+        int dp[]  =  new int[n+1];
+        dp[0] =0;
+        dp[1] = 1;
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
