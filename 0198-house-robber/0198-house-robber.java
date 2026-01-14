5b47d0c9a5d9665978c83fefdf36a4eb2391b9eb
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2026-01-14 14:11:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,43 @@
-class Solution {
-   int MaxRob(int []nums, int i ,Integer[]dp){
-        if(i >= nums.length)return 0;
-        if(dp[i] != null)return dp[i];
-        int pick = nums[i] +MaxRob(nums, i+2,dp);
+
+// memoization
+
+
+// class Solution {
+//    int MaxRob(int []nums, int i ,Integer[]dp){
+//         if(i >= nums.length)return 0;
+//         if(dp[i] != null)return dp[i];
+//         int pick = nums[i] +MaxRob(nums, i+2,dp);
       
-       int skip  = MaxRob(nums,i+1,dp);
-       return dp[i] = Math.max(pick, skip);
-    }
-    public int rob(int[] nums) {
-        Integer dp[] = new Integer[nums.length];
-        return MaxRob(nums,0,dp);
+//        int skip  = MaxRob(nums,i+1,dp);
+//        return dp[i] = Math.max(pick, skip);
+//     }
+//     public int rob(int[] nums) {
+//         Integer dp[] = new Integer[nums.length];
+//         return MaxRob(nums,0,dp);
         
+//     }
+// }
+
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
+        int n = dp.length;
+        dp[0] = nums[0];
+        for(int i =1;i<nums.length;i++){
+            
+           int pick;
+            if (i > 1) {
+                pick = nums[i] + dp[i - 2];
+            } else {
+                pick = nums[i]; 
+            }
+
+            int skip = dp[i - 1];
+            dp[i] = Math.max(pick, skip);
+        }
+        return dp[n-1];
     }
 }
