0893daa494536b7da89db189a6cc84c53d8d65e9
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2026-01-17 15:16:16 +0530

    Time: 46 ms (5.34%), Space: 48.2 MB (6.61%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minInsertions(String s) {
+        StringBuilder s1 = new StringBuilder(s);
+        s1.reverse();
+        int n = s.length();
+        int m = s1.length();
+        Integer dp[][] =  new Integer[n][m];
+        return s.length() - longPalidromicmSub(n-1,m-1,s1,s,dp);
+    }
+    private static int longPalidromicmSub(int i, int j, StringBuilder s1, String s,Integer dp[][]) {
+        if(i < 0 || j < 0)return 0;
+        if(dp[i][j] != null) return dp[i][j];
+        if(s.charAt(i) == s1.charAt(j)){
+            return dp[i][j] = 1 + longPalidromicmSub(i-1,j-1,s1,s,dp);
+        }
+        return dp[i][j] = Math.max(longPalidromicmSub(i-1,j,s1,s,dp) , longPalidromicmSub(i,j-1,s1,s,dp));
+    }
+}
