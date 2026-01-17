9471707d9404f3f5caee88e78af766b2b2d7ce64
mohit kumar singh <159545677+mohitsingh841231@users.noreply.github.com>
2026-01-17 15:00:19 +0530

    Time: 120 ms (5.1%), Space: 212.6 MB (5.36%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -5,7 +5,18 @@ class Solution {
         int n = s.length();
         int m = s1.length();
         Integer dp[][] = new Integer[n][m];
-        return longPalidromicmSub(n-1,m-1,s1,s,dp);
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(s.charAt(i) == s1.charAt(j)){
+                  dp[i][j] = 1 + ((i > 0 && j > 0) ? dp[i-1][j-1] : 0);
+                }
+                  else{
+                    dp[i][j] =  Math.max(
+                        (i > 0 ) ? dp[i-1][j] : 0, (j > 0) ? dp[i][j-1] : 0);
+                  }
+            }
+        }
+         return dp[n-1][m-1];
 
 
     }
